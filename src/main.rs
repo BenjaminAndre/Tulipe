a8f70b9f762f4d4de7f04f2d6b6db2d85c4e9871
Benjamin André <andre.vincent.benjamin@gmail.com>
2025-02-04 12:58:59 +0100

    About to delete Rust

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use notify_rust::Notification;
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
     
-    
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
+    ui.global::<Logic>().on_notify(|content| {
+        let _ = Notification::new()
+    .summary("Tulipe")
+    .body(content.as_str())
+    .show();
     });
 
     ui.run()?;
+    
+    
 
     Ok(())
 }
